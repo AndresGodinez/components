37dc36eac507fa81e6e513a2b150ed5b823b1f37
Andres Godinez <andres@barpres.com>
2020-01-27 13:15:13 -0600

    add home page temp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:using_components/src/pages/home_temp.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,17 +7,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Componentes App',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+        title: 'Componentes App',
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
           appBar: AppBar(
             title: Text('Componentes'),
           ),
-          body: Center(
-            child: Container(
-              child: Text('hello'),
-            ),
-          )),
-    );
+          body: HomeTemp(),
+        ));
   }
 }
